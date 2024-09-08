fc48ade9018f7f295ed5faba2df0cf40d03e5ae3
Aby-ss <raoabdulhadi952@gmail.com>
2024-09-08 16:52:40 +0400

    Electron App initial startup config ✅

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: __dirname + '/preload.js', // Preload script
+      nodeIntegration: true, // Allows Node.js modules to be used in the renderer
+    },
+  });
+
+  // Load index.html
+  mainWindow.loadFile('index.html');
+}
+
+// Called when Electron has finished initialization
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed (except on macOS)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+// Re-create window when dock icon is clicked (macOS)
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
