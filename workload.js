5369fe1c4c843d8ad96f2ee7ffdbed1c5782d105
Aby-ss <raoabdulhadi952@gmail.com>
2024-09-15 16:31:20 +0400

    Removed Write function ✅

--- workload.js
+++ workload.js
@@ -7,24 +7,6 @@ const auth = new google.auth.GoogleAuth({
     scopes: ['https://www.googleapis.com/auth/documents'] // Scope for Google Docs
 });
 
-// Function to write to a Google Docs document
-async function writeGoogleDocs(documentId, requests) {
-    try {
-        const docs = google.docs({ version: 'v1', auth }); // Create a Google Docs API client
-
-        // Send a batchUpdate request to modify the document
-        const writer = await docs.documents.batchUpdate({
-            documentId, // ID of the document to update
-            requestBody: {
-                requests // Array of requests detailing the changes to be made
-            }
-        });
-        return writer; // Return the response from the Google Docs API
-    } catch (error) {
-        console.error('error', error); // Log any errors that occur
-    }
-}
-
 // Function to read from a Google Docs document
 async function readGoogleDocs(documentId) {
     try {
@@ -40,17 +22,6 @@ async function readGoogleDocs(documentId) {
 
 // Self-invoking async function to execute the read and write operations
 (async () => {
-    // Example of writing to a document
-    const writer = await writeGoogleDocs('11SbzfM5FgyJeoBOrBT35rMZNfQQLsiY5PkPB39aYIuw', [{
-        insertText: {
-            location: {
-                index: 1 // Specify the index to insert text
-            },
-            text: "Hello World\n" // Text to be inserted
-        }
-    }]);
-    console.log(writer); // Log the response from the write operation
-
     // Example of reading from a document
     const data = await readGoogleDocs('11SbzfM5FgyJeoBOrBT35rMZNfQQLsiY5PkPB39aYIuw');
     // Extract and log the text content from the document
